f5e1167bf00da640b45c88d99f8e9752e1700878
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-14 11:12:03 +0200

    Buchliste

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  readonly books = signal<Book[]>([]);
 
+  constructor() {
+    this.books.set([
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 42.9,
+        rating: 5
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        price: 36.9,
+        rating: 3
+      },
+    ]);
+  }
 }
 
 
