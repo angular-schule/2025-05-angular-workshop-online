006e3f10ec4583c35654f62cec0d3ed5cf9e729c
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-14 10:36:55 +0200

    Dashboard anlegen

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'book-rating';
+  title = 'Book Rating!';
 }
