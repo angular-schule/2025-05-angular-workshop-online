a1aaa189d4aa7ed7fa889266eee1cc47305cb68b
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-15 11:06:09 +0200

    Buchliste aktualisieren

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookComponent } from "../book/book.component";
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,8 @@ import { BookComponent } from "../book/book.component";
 })
 export class DashboardComponent {
   readonly books = signal<Book[]>([]);
+
+  #rs = inject(BookRatingService);
 
   constructor() {
     this.books.set([
@@ -31,11 +34,32 @@ export class DashboardComponent {
   }
 
   doRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.#rs.rateUp(book);
+    this.#updateList(ratedBook);
   }
 
 
   doRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.#rs.rateDown(book);
+    this.#updateList(ratedBook);
+  }
+
+  #updateList(ratedBook: Book) {
+    this.books.update(oldList => {
+      // [1,2,3,4,5].map(e => e * 10) // [10, 20, 30, 40, 50]
+      // [1,2,3,4,5,6,7,8,9].filter(e => e > 5) // [6,7,8,9]
+
+      // const newList = oldList.filter(b => b.isbn !== ratedBook.isbn);
+      // newList.push(ratedBook);
+      // return newList;
+
+      return oldList.map(b => {
+        if (b.isbn === ratedBook.isbn) {
+          return ratedBook;
+        } else {
+          return b;
+        }
+      });
+    });
   }
 }
