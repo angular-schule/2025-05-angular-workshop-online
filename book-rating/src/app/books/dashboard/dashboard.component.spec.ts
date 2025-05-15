b07058956af5d0efd1e456179ef7799f58ac6335
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-15 11:21:45 +0200

    Test: Mock und Provider für Service

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const ratingMock = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [
+        // BRS ersetzen: Immer wenn jemand BRS anfordert,
+        // wird stattdessen ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
 
