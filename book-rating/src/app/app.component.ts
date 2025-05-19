fc8b6ef92982e0b02474c6fcfa444f4002625eb6
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-19 10:56:41 +0200

    Routen, Outlet, Redirect, Wildcard

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { booksRoutes } from './books/books.routes';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // bei Weiterleitung vom leeren Pfad:
+  // (fast) immer pathMatch:full nötig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+  { path: '**', component: ErrorPageComponent }
+];
