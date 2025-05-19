357defd3915990b3ffa922384bc0dd0034eebff6
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-19 12:26:50 +0200

    Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
@@ -8,5 +10,30 @@ import { RouterLink } from '@angular/router';
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
+  #route = inject(ActivatedRoute);
+  #bs = inject(BookStoreService);
 
+  readonly book = signal<Book | undefined>(undefined);
+
+  constructor() {
+    // Pull
+    // const isbn = this.#route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+    // console.log(isbn);
+
+    // Push
+    this.#route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion – gefährlich!
+
+      this.#bs.getSingle(isbn).subscribe(receivedBook => {
+        this.book.set(receivedBook);
+      });
+    });
+  }
 }
+
+
+/*
+- ISBN aus der URL auslesen
+- HTTP für das Buch
+- Anzeigen
+*/
