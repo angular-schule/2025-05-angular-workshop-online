2fe761c6ef51f9923313877e02f07b9cf01d68c5
Ferdinand Malcher <ferdinand@malcher.media>
2025-05-20 14:22:37 +0200

    RxJS: Intro

--- rxjs-playground/src/app/exercise-creating/exercise-creating.ts
+++ rxjs-playground/src/app/exercise-creating/exercise-creating.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Subscriber, Observer } from 'rxjs';
 
 import { HistoryWindow } from '../shared/history-window/history-window';
 
@@ -24,12 +24,60 @@ export class ExerciseCreating {
 
     /******************************/
 
-    
+
+    // Producer: generiert die Daten
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+      sub.next(10);
+      sub.next(20);
+      sub.next(30);
+
+      setTimeout(() => sub.next(100), 2000)
+      setTimeout(() => sub.next(400), 4000)
+      setTimeout(() => sub.complete(), 6000)
+    }
+
+    // Observer: hört von außen zu
+    const obs: Observer<number> = {
+      next: value => console.log(value),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG')
+    }
+
+    // producer(obs);
+    // Observable: Schnittstelle zwischen Producer und Observer
+    // $ Finnische Notation
+    const myObs$ = new Observable(producer);
+
+    myObs$.subscribe(obs);
+    myObs$.subscribe(obs);
+    myObs$.subscribe(obs);
+
+
+    function httpGetReq(url: string) {
+      return new Observable(sub => {
+        fetch(url)
+          .then(res => res.json())
+          .then(data => {
+            sub.next(data);
+          })
+      })
+    }
+
     /******************************/
   }
 
   log(msg: unknown) {
     this.logStream$.next(msg);
+  }
+
+  async getData() {
+    const data = await fetch('https://api.angular.schule');
+    console.log(data);
+
+
+    fetch('https://api.angular.schule').then(data => console.log(data));
   }
 
 }
